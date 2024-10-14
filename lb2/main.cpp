ceccac4a1c9ebc4ed926e077945066fc6d302275
Boris Prokhorov <b.prokhorov@yadro.com>
2024-10-14 16:55:19 +0300

    lb2 done

--- lb2/main.cpp
+++ lb2/main.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <iostream>
 #include <random>
+#include <sstream>
+#include <string>
 #include <vector>
 
 void sequentialSelectionSort(std::vector<int>& arr) {
@@ -17,15 +19,61 @@ void sequentialSelectionSort(std::vector<int>& arr) {
    }
 }
 
-void parallelSelectionSort(std::vector<int>& arr, int rank, int size) {
+int smallest(int* arr, int start, int size) {
+   int minIdx = start;
+   for (int i = start + 1; i < size; i++) {
+      if (arr[minIdx] > arr[i]) minIdx = i;
+   }
+   std::swap(arr[start], arr[minIdx]);
+   return arr[start];
+}
+
+void parallelSelectionSort(std::vector<int>& arr, int rank, int size,
+                           int world_size) {
    int n = arr.size();
-   int local_size = n / size;
+   int local_size = n / world_size;
+
    std::vector<int> local_data(local_size);
 
    MPI_Scatter(arr.data(), local_size, MPI_INT, local_data.data(), local_size,
                MPI_INT, 0, MPI_COMM_WORLD);
 
-   sequentialSelectionSort(local_data);
+   // Local sorting for each part of the array
+   int local_min = smallest(local_data.data(), 0, local_size);
+
+   int global_min;
+   int process_num;
+
+   for (int step = 0; step < n; step++) {
+      if (rank == 0) {
+         global_min = local_min;
+         process_num = 0;
+      }
+
+      for (int i = 1; i < world_size; i++) {
+         if (rank == 0) {
+            int received_min;
+            MPI_Recv(&received_min, 1, MPI_INT, i, 0, MPI_COMM_WORLD,
+                     MPI_STATUS_IGNORE);
+            if (received_min < global_min) {
+               global_min = received_min;
+               process_num = i;
+            }
+         } else if (rank == i) {
+            MPI_Send(&local_min, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+         }
+      }
+
+      MPI_Bcast(&process_num, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+      if (rank == process_num) {
+         local_min = smallest(
+             local_data.data(), 1,
+             local_size);  // Updating the minimum element for this process
+      }
+
+      MPI_Barrier(MPI_COMM_WORLD);
+   }
 
    MPI_Gather(local_data.data(), local_size, MPI_INT, arr.data(), local_size,
               MPI_INT, 0, MPI_COMM_WORLD);
@@ -38,6 +86,13 @@ double measure_time(Function fn) {
    auto end = std::chrono::high_resolution_clock::now();
    std::chrono::duration<double> duration = end - start;
    return duration.count();
+}
+
+template <typename... Args>
+std::string format_string(const Args&... args) {
+   std::ostringstream oss;
+   (oss << ... << args);
+   return oss.str();
 }
 
 std::vector<int> generate_random_data(int size) {
@@ -54,31 +109,39 @@ std::vector<int> generate_random_data(int size) {
 int main(int argc, char** argv) {
    MPI_Init(&argc, &argv);
 
-   int rank, size;
+   int rank, world_size;
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-   MPI_Comm_size(MPI_COMM_WORLD, &size);
+   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
    std::vector<int> data_sizes = {10, 100, 500, 1000, 5000};
 
+   std::string output_filename =
+       format_string("measure111ments_", world_size, ".csv");
+   std::string output_path = format_string("./measurements/", output_filename);
+
    if (rank == 0) {
-      std::ofstream outfile("times.csv");
+      std::ofstream outfile(output_path);
       outfile << "size,sequential_time,parallel_time\n";
+   }
 
-      for (int n : data_sizes) {
-         std::vector<int> data = generate_random_data(n);
+   for (int n : data_sizes) {
+      std::vector<int> data = generate_random_data(n);
+      std::vector<int> parallel_data = data;
 
-         std::vector<int> parallel_data = data;
+      // Measure the running time of the sequential algorithm
+      double sequential_time =
+          measure_time([&]() { sequentialSelectionSort(data); });
 
-         double sequential_time =
-             measure_time([&]() { sequentialSelectionSort(data); });
+      // Measure the running time of the parallel algorithm
+      double parallel_time = measure_time(
+          [&]() { parallelSelectionSort(parallel_data, rank, n, world_size); });
 
-         double parallel_time = measure_time(
-             [&]() { parallelSelectionSort(parallel_data, rank, size); });
-
+      if (rank == 0) {
+         std::ofstream outfile(output_path, std::ios_base::app);
          outfile << n << "," << sequential_time << "," << parallel_time << "\n";
       }
 
-      outfile.close();
+      MPI_Barrier(MPI_COMM_WORLD);
    }
 
    MPI_Finalize();
--- lb2/plot.py
+++ lb2/plot.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('times.csv')
+proccess_counts = [1, 2, 3, 4]
 
-plt.plot(df['size'], df['sequential_time'], label='Последовательная сортировка выбором')
-plt.plot(df['size'], df['parallel_time'], label='Параллельная сортировка выбором')
+for number in proccess_counts:
+  df = pd.read_csv(f'./measurements/measurements_{number}.csv')
 
-plt.xlabel('Размер массива')
-plt.ylabel('Время выполнения (секунды)')
-plt.title('Зависимость времени выполнения от размерности цепочки данных')
-plt.legend()
-plt.grid(True)
-plt.savefig('measurements.png')
+  plt.plot(df['size'], df['sequential_time'], label='Последовательная сортировка выбором')
+  plt.plot(df['size'], df['parallel_time'], label=f'Параллельная сортировка выбором (количество процессов {number})')
+  
+
+  plt.xlabel('Размер массива')
+  plt.ylabel('Время выполнения (секунды)')
+  plt.title(f'Зависимость времени выполнения от размерности цепочки данных')
+  plt.legend(loc='upper left')
+  plt.grid(True)
+  plt.savefig(f'./plots/plot_{number}.png')
+  plt.close()
