aff987cdb8a679e667d544aa2074a52d3e9fc567
Boris Prokhorov <b.prokhorov@yadro.com>
2024-12-18 18:13:45 +0300

    cw

---
+++ cw/index.js
@@ -0,0 +1,36 @@
+import soap from 'soap';
+import axios from 'axios';
+
+const SOAP_URL =
+  'https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL';
+const REST_URL = 'https://jsonplaceholder.typicode.com/todos/1';
+
+async function getSOAPData() {
+  try {
+    const client = await soap.createClientAsync(SOAP_URL);
+    const args = { ubiNum: 42 };
+    const result = await client.NumberToWordsAsync(args);
+    console.log('SOAP Response:', result[0].NumberToWordsResult);
+  } catch (error) {
+    console.error('SOAP Error:', error);
+  }
+}
+
+async function getRESTData() {
+  try {
+    const response = await axios.get(REST_URL);
+    console.log('REST Response:', response.data);
+  } catch (error) {
+    console.error('REST Error:', error);
+  }
+}
+
+async function main() {
+  console.log('Fetching data from SOAP service...');
+  await getSOAPData();
+
+  console.log('Fetching data from REST service...');
+  await getRESTData();
+}
+
+main();
